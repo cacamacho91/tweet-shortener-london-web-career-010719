ca8f41ef0222d53238607b701c2eb7a124ee0260
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-29 16:25:21 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter (tweet)
+  tweet_array = tweet.split(" ")
+
+  #iterate over words
+  tweet_array.collect do |word|
+    if dictionary.keys.include?(word)
+      
+    else
+      word
+    end
+  end
+end
